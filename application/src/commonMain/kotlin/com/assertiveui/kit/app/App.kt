a8b6d519bd8670ad77be47de3777b9027fdde5f0
Stoyan Vuchev <stoyan.v.vuchev@gmail.com>
2025-04-20 18:56:43 +0300

    Added collapsable top bar with interactive action buttons.
    - Added a collapsable top bar component which animates its size and content based on scroll position.
    - Added interactive action buttons which change their appearance on hover and press events.
    - Added icons for navigation, search, and menu actions.
    - Fixed transformFraction utility name to `TransformationUtils.kt`.
    - Improved type styles for titles.
    - Improved example app usage of `FoundationLayout` by using a collapsable top bar.

--- application/src/commonMain/kotlin/com/assertiveui/kit/app/App.kt
+++ application/src/commonMain/kotlin/com/assertiveui/kit/app/App.kt
@@ -36,6 +36,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
@@ -51,18 +52,26 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import assertiveuikit.application.generated.resources.Res
+import assertiveuikit.application.generated.resources.arrow_left
+import assertiveuikit.application.generated.resources.menu
+import assertiveuikit.application.generated.resources.search
 import com.assertiveui.kit.app.core.ui.AppUIWrapper
 import com.assertiveui.kit.core.components.layout.foundation.FoundationLayout
+import com.assertiveui.kit.core.components.topbar.CollapsingTopBar
+import com.assertiveui.kit.core.components.topbar.TopBarUtils
+import com.assertiveui.kit.core.components.topbar.action.TopBarAction
 import com.assertiveui.kit.core.theme.Theme
 import com.assertiveui.kit.core.theme.ThemeMode
 import com.assertiveui.kit.core.theme.color.palette.ColorPaletteModel
 import com.assertiveui.kit.core.theme.color.palette.LightColorPaletteTokens
-import com.assertiveui.kit.core.theme.color.palette.LocalContentColor
 import com.assertiveui.kit.core.theme.color.palette.asAnimatedColorPalette
 import com.assertiveui.kit.core.theme.color.utils.ColorUtils
 import com.assertiveui.kit.core.theme.isInDarkThemeMode
+import org.jetbrains.compose.resources.painterResource
 import kotlin.math.roundToInt
 
 @Composable
@@ -108,33 +117,52 @@ fun App(onApplyDarkIcons: (Boolean) -> Unit = remember { {} }) {
         colorPalette = colorPalette
     ) {
 
+        val scrollBehavior = TopBarUtils.exitUntilCollapsedScrollBehavior()
+
         FoundationLayout(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
             topBar = {
 
-                Box(
-                    modifier = Modifier
-                        .padding(
-                            WindowInsets.systemBars.exclude(
-                                WindowInsets.systemBars.only(
-                                    WindowInsetsSides.Bottom
-                                        .plus(WindowInsetsSides.Start)
-                                )
-                            ).asPaddingValues()
-                        )
-                        .fillMaxWidth()
-                        .height(64.dp),
-                    contentAlignment = Alignment.CenterStart
-                ) {
+                CollapsingTopBar(
+                    title = "Top Bar",
+                    scrollBehavior = scrollBehavior,
+                    primaryAction = {
 
-                    BasicText(
-                        modifier = Modifier.padding(horizontal = 32.dp),
-                        text = "Top Bar",
-                        style = Theme.typefaces.titleSmall.copy(
-                            color = LocalContentColor.current
+                        TopBarAction(
+                            icon = painterResource(Res.drawable.arrow_left),
+                            onClick = remember { {} },
+                            contentDescription = "Navigate Up"
                         )
-                    )
 
-                }
+                    },
+                    secondaryActions = {
+
+                        TopBarAction(
+                            icon = painterResource(Res.drawable.search),
+                            onClick = remember { {} },
+                            contentDescription = "Search"
+                        )
+
+                        TopBarAction(
+                            icon = painterResource(Res.drawable.menu),
+                            onClick = remember { {} },
+                            contentDescription = "Menu"
+                        )
+
+                    }/*,
+                    decoration = { progress ->
+
+                        Image(
+                            modifier = Modifier
+                                .graphicsLayer { this.alpha = 1f - progress }
+                                .matchParentSize(),
+                            contentScale = ContentScale.Crop,
+                            contentDescription = null,
+                            painter = painterResource(Res.drawable.bg)
+                        )
+
+                    }*/
+                )
 
             },
             bottomBar = {
@@ -196,103 +224,123 @@ fun App(onApplyDarkIcons: (Boolean) -> Unit = remember { {} }) {
             }
         ) { safePadding ->
 
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(safePadding),
-                contentAlignment = Alignment.Center
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = safePadding
             ) {
 
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 32.dp),
-                    verticalArrangement = Arrangement.Bottom
-                ) {
+                item {
 
-                    var newHue by remember(hue) { mutableIntStateOf(hue) }
-                    val newHueValue by rememberUpdatedState(newHue.toFloat() / 360f)
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 32.dp),
+                        verticalArrangement = Arrangement.Bottom
+                    ) {
+
+                        Spacer(modifier = Modifier.height(32.dp))
+
+                        var newHue by remember(hue) { mutableIntStateOf(hue) }
+                        val newHueValue by rememberUpdatedState(newHue.toFloat() / 360f)
+
+                        BasicText(
+                            text = "Hue:  $newHue",
+                            style = Theme.typefaces.bodyLarge.copy(
+                                color = Theme.colorPalette.onSurfaceElevationLow
+                            )
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Slider(
+                            modifier = Modifier
+                                .widthIn(
+                                    min = 256.dp,
+                                    max = 512.dp
+                                ),
+                            value = newHueValue,
+                            onValueChange = { newHueFloat ->
+                                newHue = (newHueFloat * 360f).roundToInt()
+                            },
+                            onValueChangeFinished = { onHueChanged(newHue) },
+                            colors = SliderDefaults.colors(
+                                thumbColor = Theme.colorPalette.base,
+                                activeTrackColor = Theme.colorPalette.base,
+                                inactiveTrackColor = Theme.colorPalette.outline,
+                            )
+                        )
+
+                        Spacer(modifier = Modifier.height(32.dp))
+
+                        Row(modifier = Modifier.fillMaxWidth()) {
+
+                            Box(
+                                modifier = Modifier
+                                    .clip(Theme.shapes.smallShape)
+                                    .background(Theme.colorPalette.base)
+                                    .clickable(onClick = onThemeMode)
+                                    .padding(
+                                        horizontal = 18.dp,
+                                        vertical = 12.dp
+                                    )
+                                    .animateContentSize(),
+                                content = {
+
+                                    BasicText(
+                                        text = "$themeMode Theme",
+                                        style = Theme.typefaces.actionLarge.copy(
+                                            color = Theme.colorPalette.onBase
+                                        )
+                                    )
+
+                                }
+                            )
+
+                            Spacer(modifier = Modifier.width(8.dp))
+
+                            val onReset = remember {
+                                { onHueChanged(LightColorPaletteTokens.baseHue) }
+                            }
+
+                            Box(
+                                modifier = Modifier
+                                    .clip(Theme.shapes.smallShape)
+                                    .clickable(onClick = onReset)
+                                    .padding(
+                                        horizontal = 18.dp,
+                                        vertical = 12.dp
+                                    ),
+                                content = {
+
+                                    BasicText(
+                                        text = "Reset Hue",
+                                        style = Theme.typefaces.actionLarge.copy(
+                                            color = Theme.colorPalette.base
+                                        )
+                                    )
+
+                                }
+                            )
+
+                        }
+
+                    }
+
+                }
+
+                items(100) {
 
                     BasicText(
-                        text = "Hue:  $newHue",
-                        style = Theme.typefaces.bodyLarge.copy(
+                        modifier = Modifier
+                            .padding(
+                                horizontal = 32.dp,
+                                vertical = 8.dp
+                            ),
+                        text = "Item: ${it + 1}",
+                        style = Theme.typefaces.bodyMedium.copy(
                             color = Theme.colorPalette.onSurfaceElevationLow
                         )
                     )
-
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    Slider(
-                        modifier = Modifier
-                            .widthIn(
-                                min = 256.dp,
-                                max = 512.dp
-                            ),
-                        value = newHueValue,
-                        onValueChange = { newHueFloat ->
-                            newHue = (newHueFloat * 360f).roundToInt()
-                        },
-                        onValueChangeFinished = { onHueChanged(newHue) },
-                        colors = SliderDefaults.colors(
-                            thumbColor = Theme.colorPalette.base,
-                            activeTrackColor = Theme.colorPalette.base,
-                            inactiveTrackColor = Theme.colorPalette.outline,
-                        )
-                    )
-
-                    Spacer(modifier = Modifier.height(32.dp))
-
-                    Row(modifier = Modifier.fillMaxWidth()) {
-
-                        Box(
-                            modifier = Modifier
-                                .clip(Theme.shapes.smallShape)
-                                .background(Theme.colorPalette.base)
-                                .clickable(onClick = onThemeMode)
-                                .padding(
-                                    horizontal = 18.dp,
-                                    vertical = 12.dp
-                                )
-                                .animateContentSize(),
-                            content = {
-
-                                BasicText(
-                                    text = "$themeMode Theme",
-                                    style = Theme.typefaces.actionLarge.copy(
-                                        color = Theme.colorPalette.onBase
-                                    )
-                                )
-
-                            }
-                        )
-
-                        Spacer(modifier = Modifier.width(8.dp))
-
-                        val onReset = remember {
-                            { onHueChanged(LightColorPaletteTokens.baseHue) }
-                        }
-
-                        Box(
-                            modifier = Modifier
-                                .clip(Theme.shapes.smallShape)
-                                .clickable(onClick = onReset)
-                                .padding(
-                                    horizontal = 18.dp,
-                                    vertical = 12.dp
-                                ),
-                            content = {
-
-                                BasicText(
-                                    text = "Reset Hue",
-                                    style = Theme.typefaces.actionLarge.copy(
-                                        color = Theme.colorPalette.base
-                                    )
-                                )
-
-                            }
-                        )
-
-                    }
 
                 }
 
